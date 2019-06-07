d0cc3f30848ada5f19e48f9ff13da73a66700181
Raul Grados <gnr2092@gmail.com>
2019-06-07 17:48:09 -0500

    Relations in post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+    // Relacion de uno a muchos inversa
+
+    public function user(){
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function category(){
+        return $this->belongsTo('App\Category', 'category_id');
+    }
 }
